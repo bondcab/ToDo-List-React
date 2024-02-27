55937956d49df3dbc7902bad58c5b93425e1b7d3
bondcab <136003551+bondcab@users.noreply.github.com>
2024-02-27 17:08:23 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Heading from "./Components/Heading";
+import AddNewList from "./Components/AddNewList";
+import ListsContainer from "./Components/ListsContainer";
 
 function App() {
+  const [newList, setNewList] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="appContainer">
+      <Heading />
+      <AddNewList setNewList={setNewList} />
+      <ListsContainer newList={newList} setNewList={setNewList} />
     </div>
   );
 }
