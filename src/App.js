4a2eff54b68a290dcb3e7848e92c41af833a763f
bondcab <136003551+bondcab@users.noreply.github.com>
2024-02-29 19:51:41 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import ListsContainer from "./Components/ListsContainer";
 
 function App() {
   const [newList, setNewList] = useState(true);
+  const [toDoLists, setToDoLists] = useState([]);
+
+  const toDoListStorage = JSON.parse(localStorage.getItem("ToDo Lists"));
+  console.log(toDoListStorage);
 
   return (
     <div className="appContainer">
       <Heading />
-      <AddNewList setNewList={setNewList} />
+      <AddNewList setNewList={setNewList} toDoLists={toDoLists} />
       <ListsContainer newList={newList} setNewList={setNewList} />
     </div>
   );
