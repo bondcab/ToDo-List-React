bfdd1110456045c7e2578a58b01fc02a9d796aae
bondcab <136003551+bondcab@users.noreply.github.com>
2024-03-01 18:13:57 +0000

    Update ListsContainer.jsx

--- src/Components/ListsContainer.jsx
+++ src/Components/ListsContainer.jsx
@@ -1,10 +1,30 @@
 import CreateList from "./CreateList";
 
-function ListsContainer({ newList, setNewList }) {
+function ListsContainer({ newList, setNewList, setToDoLists, toDoLists }) {
+  const toDoListStorage = JSON.parse(localStorage.getItem("ToDo Lists"));
+  console.log("ToDo List in Storage", toDoListStorage);
   return (
     <div className="listsContainer">
       {" "}
-      {newList ? <CreateList setNewList={setNewList} /> : null}
+      {newList ? (
+        <CreateList
+          setNewList={setNewList}
+          setToDoLists={setToDoLists}
+          toDoLists={toDoLists}
+        />
+      ) : null}
+      {toDoListStorage
+        ? toDoListStorage.map((list) => (
+            <div className="list" key={list.id}>
+              <h2>{list.name}</h2>
+              <ul>
+                {list.tasks.map((task) => (
+                  <li>{task}</li>
+                ))}
+              </ul>
+            </div>
+          ))
+        : null}
     </div>
   );
 }
