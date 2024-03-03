4982c06034e3e328a260370a7ec9336d80b155fe
bondcab <136003551+bondcab@users.noreply.github.com>
2024-03-03 20:20:38 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Heading from "./Components/Heading";
 import AddNewList from "./Components/AddNewList";
 import ListsContainer from "./Components/ListsContainer";
 
 function App() {
-  const [newList, setNewList] = useState(false);
-  const [toDoLists, setToDoLists] = useState([]);
-
-  // const toDoListStorage = JSON.parse(localStorage.getItem("ToDo Lists"));
-  // console.log(toDoListStorage);
-  // console.log("ToDo Lists: ", toDoLists);
-
-  // useEffect(() => {
-  //   // localStorage.setItem("ToDo Lists", JSON.stringify(toDoLists));
-  //   const toDoListStorage = JSON.parse(localStorage.getItem("ToDo Lists"));
-  //   console.log("ToDo List in Storage", toDoListStorage);
-
-  // }, [toDoLists]);
+  const [newList, setNewList] = useState(false); // If set to true then CreateList component will be displayed
+  const [editList, setEditList] = useState(false); // If set to true then EditList compoent will be displayed
+  const [toDoLists, setToDoLists] = useState([]); //
+  const [editListName, setEditListName] = useState("");
 
   return (
     <div className="appContainer">
@@ -27,6 +18,10 @@ function App() {
         setNewList={setNewList}
         setToDoLists={setToDoLists}
         toDoLists={toDoLists}
+        editList={editList}
+        setEditList={setEditList}
+        setEditListName={setEditListName}
+        editListName={editListName}
       />
     </div>
   );
